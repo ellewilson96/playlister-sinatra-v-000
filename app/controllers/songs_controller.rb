47ee56197a7c60bc00019a4cee329e9d12c4aadd
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-19 19:59:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
-  post '/songs'do 
+  post '/songs'do
   @song = Song.create(:name => params["name"])
   @song.artist = Artist.find_or_create_by(:name => params["artist_name"])
 
